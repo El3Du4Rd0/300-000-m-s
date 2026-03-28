9b7caf6c4bac1e098f02b2445b8ae8de7710e84c
Pablo H <h.h.m.pablo@gmail.com>
2026-03-28 17:28:58 -0600

    Codigo para modelo jugador atributos

--- Assets/Model/Prefabs/bojack.model.cs
+++ Assets/Model/Prefabs/bojack.model.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class ConfigFuerza : MonoBehaviour
 {
-    public float velocidadInicial = 100f;
+	public float modedasInicial = 2;
+	public float velocidaMaximaInicial = 10f;
+	public float reboteInicial = 1;
+	public float velocidadInicial = 10f;
+	public float vidasInicial = 2;
+
+	public float monedasActual = 2;
+	public float velocidaMaximaActual = 10f;
+	public float reboteActual = 1;
+	public float velocidadActual = 10f;
+	public float vidasActual = 2;
 }
